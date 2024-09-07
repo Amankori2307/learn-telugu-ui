748364a4100955a8430ffe6690943c7901484b22
Aman Kori <aman@napqueens.com>
2024-09-08 01:41:55 +0530

    Added dark theme

--- src/custom-components/word-card/word-card.tsx
+++ src/custom-components/word-card/word-card.tsx
@@ -14,17 +14,19 @@ interface IWordCardProps {
 function WordCard(props: IWordCardProps) {
   const { word } = props;
   return (
-    <Card className="w-[350px]">
+    <Card className="w-[350px] bg-gray-800 text-white">
       <CardHeader>
-        <CardTitle>
+        <CardTitle className="text-lg font-semibold text-white">
           {word.word} ({word.meaning})
         </CardTitle>
-        <CardDescription>{word.pronunciation}</CardDescription>
+        <CardDescription className="text-gray-400">
+          {word.pronunciation}
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <ul>
           {word.examples.map((example, index) => (
-            <li className="text-xs" key={index}>
+            <li className="text-xs text-gray-300" key={index}>
               {example}
             </li>
           ))}
--- src/custom-components/word-carousel/word-carousel.tsx
+++ src/custom-components/word-carousel/word-carousel.tsx
@@ -1,8 +1,3 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
 import commonUtils from "@/utils/common.utils";
 import { useMemo } from "react";
 import WordCard from "../word-card/word-card";
@@ -10,19 +5,15 @@ import WordCard from "../word-card/word-card";
 function WordCarousel() {
   const data = useMemo(() => commonUtils.getAllDataAsArray(), []);
   return (
-    <Carousel className="w-full max-w-sm">
-      <CarouselContent className="-ml-1">
-        {data.map((word, index) => (
-          <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <WordCard word={word} />
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      {/* <CarouselPrevious /> */}
-      {/* <CarouselNext /> */}
-    </Carousel>
+    <div className="bg-gray-900 py-5">
+      {data.map((word, index) => (
+        <div key={index} className="px-2 py-3 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <WordCard word={word} />
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
