5e594ca96dc016ba09c903077188ec0dd55e3fda
Aman Kori <aman@napqueens.com>
2024-09-08 13:19:41 +0530

    Updated routing

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/learn-telugu-ui">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
