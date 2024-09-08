6f458756db37983918c3c97d809e92f5a868e137
Aman Kori <aman@napqueens.com>
2024-09-08 13:24:13 +0530

    Updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,11 @@ import WordListWrapper from "./custom-components/word-list-wrapper/word-list-wra
 
 function App() {
   return (
-    // <div className="app">
     <Routes>
-      <Route path="/" element={<WordCarousel />} />
       <Route path="/word-carousel" element={<WordCarousel />} />
       <Route path="/word-list" element={<WordListWrapper />} />
+      <Route path="/" element={<WordCarousel />} />
     </Routes>
-    // </div >
   );
 }
 
